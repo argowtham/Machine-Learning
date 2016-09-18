57bccfb903571953dd533057613cb7718de1cb8c
argowtham <gowtayna@indiana.edu>
2016-09-18 02:43:16 -0400

    Implemented until depth-1 level. Prediction part completed.

--- Decision_trees/decision_tree.py
+++ Decision_trees/decision_tree.py
@@ -6,17 +6,62 @@ from Decision_trees import node
 
 class DecisionTree:
     """Class for specifying the properties of decision tree"""
-    def __init__(self, a, b):
+    def __init__(self, a, b, features):
         self.depth = a
-        self.features = ['class', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'Id']
-        self.input = pd.read_table(b, delim_whitespace=True, names=self.features, header=None)
+        self.features = features
+        self.input = b
+        self.root = None
 
     def display(self):
         print("Depth: ", self.depth)
         print("Input file: ", self.input)
         print("Features: ", self.features)
 
-    def find_node(self, data, predictor):
+    def build_tree(self):
+        input_features = self.features[1:7]
+        data = self.input
+        self.root = find_node(data, input_features)
+        self.root.depth = 1
+        node_list = [self.root]
+        for current_node in node_list:
+            if current_node.depth <= self.depth:
+                node_list.extend(find_child(data, current_node, current_node.child, input_features))
+            #     print(current_node.name, current_node.depth)
+            #     for kid in current_node.child:
+            #         print(current_node.child[kid].parent.name, kid, current_node.child[kid].name)
+            # else:
+            #     print(current_node.name, current_node.depth, current_node.label)
+
+    def predict(self, data):
+        data['predicted'] = '-1'
+        for i in range(len(data)):
+            node_list = [self.root]
+            for item in node_list:
+                x = data.get_value(i, item.name)
+                node_list.append(item.child[x])
+                if item.label is not None:
+                    data.set_value(i, 'predicted', item.label)
+                    break
+        return data
+
+        # input_features.remove(root.name)
+        # for key in current_node.child:
+        #     sub_data = data[data[current_node.name].isin([key])]
+        #     if self.depth == current_node.depth:
+        #         current_node.label = set_label(sub_data)
+        #         print(current_node.name, key, current_node.label)
+        #         continue
+        #     input_features.remove(current_node.name)
+        #     current_node.child[key] = self.find_node(sub_data, input_features)
+        #     child_node = current_node.child[key]
+        #     child_node.parent = current_node
+        #     child_node.depth = current_node.depth + 1
+        #     del data[current_node.name]
+        #     print(current_node.child[key].parent.name, key, current_node.child[key].name)
+        #     current_node = child_node
+
+
+def find_node(data, predictor):
         gain, unique_values = {}, []
         for feature in predictor:
             entropy, entropy_sum = calculate_entropy(data), 0
@@ -28,33 +73,35 @@ class DecisionTree:
             gain[feature] = calculate_gain(entropy, entropy_sum)
         sorted_gain = sorted(gain.items(), key=operator.itemgetter(1))
         sorted_gain.reverse()
+        print(sorted_gain)
         best_attribute = sorted_gain[0][0]
         attribute_values = list(set(data[best_attribute]))
         current_node = node.Node(best_attribute, attribute_values)
         return current_node
 
-    def build_tree(self):
-        input_features = self.features[1:7]
-        data = self.input
-        root = self.find_node(data, input_features)
-        root.depth = 1
-        current_node = root
-        print(root.name)
-        # input_features.remove(root.name)
-        for key in current_node.child:
-            sub_data = data[data[current_node.name].isin([key])]
-            if self.depth == current_node.depth:
-                current_node.label = set_label(sub_data)
-                print(key, current_node.label)
-                continue
-            input_features.remove(current_node.name)
-            current_node.child[key] = self.find_node(sub_data, input_features)
-            child_node = current_node.child[key]
-            child_node.parent = current_node
-            child_node.depth = current_node.depth + 1
-            del data[current_node.name]
-            print(current_node.child[key].parent.name, key, current_node.child[key].name)
-            current_node = child_node
+
+def find_child(data, current_node, possible_values, features):
+    edited_features = list(features)
+    parents = [current_node]
+    # edited_features.remove(current_node.name)
+    for i in parents:
+        edited_features.remove(i.name)
+        if i.parent is None:
+            break
+        parents.append(i.parent)
+    child_list = []
+    for value in possible_values:
+        sub_data = data[data[current_node.name].isin([value])]
+        for attribute in parents:
+            del sub_data[attribute.name]
+        current_node.child[value] = find_node(sub_data, edited_features)
+        child_node = current_node.child[value]
+        child_list.append(child_node)
+        child_node.parent = current_node
+        child_node.depth = current_node.depth + 1
+        if child_node.depth > dt.depth:
+            child_node.label = set_label(sub_data)
+    return child_list
 
 
 def set_label(data):
@@ -91,14 +138,30 @@ def calculate_gain(entropy1, entropy2):
     return entropy1 - entropy2
 
 
+def process_data(file_name, attr):
+    return pd.read_table(file_name, delim_whitespace=True, names=attr, header=None)
+
+
+def find_error(data, a, b):
+    actual = list(data[a])
+    predicted = list(data[b])
+    error = 0
+    for i in range(len(actual)):
+        if not actual[i] == predicted[i]:
+            error += 1
+    return error
+
 if __name__ == '__main__':
     print("Program to implement the decision tree without using package")
     depth = eval(input("Enter the desired depth of the decision tree\n"))
     path = input("Enter the filename with the full path\n")
-    dt = DecisionTree(depth, path)
+    attributes = ['class', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'Id']
+    dt = DecisionTree(depth, process_data(path, attributes), attributes)
     # dt.display()
     dt.build_tree()
-
+    test_data = input("Enter the test file path")
+    predicted_data = dt.predict(process_data(test_data, attributes))
+    print("Number of misclassified data points:", find_error(predicted_data, 'class', 'predicted'))
 
     # for file in train_files:
     #     data = pd.read_table(path+'\/'+file, delim_whitespace=True, names=features, header=None)
