8bad0d18eb203f73ab7a560db3db1c8d84befcc6
Gowtham Ayna Raveendran <gowtayna@indiana.edu>
2016-09-18 23:26:24 -0400

    Delete node.py

--- Decision_trees/node.py
+++
@@ -1,9 +0,0 @@
-class Node:
-    def __init__(self, name, child):
-        self.name = name
-        self.child = {}
-        for kid in child:
-            self.child[kid] = None
-        self.parent = None
-        self.label = None
-        self.depth = None
