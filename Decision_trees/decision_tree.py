fe35adb33444594c8bf47d4adffd43db9af77be3
argowtham <gowtayna@indiana.edu>
2016-09-24 14:49:32 -0400

    Multiclass classifier  implemented using decision tree algorithm

--- Decision_trees/decision_tree.py
+++ Decision_trees/decision_tree.py
@@ -21,28 +21,56 @@ class DecisionTree:
         input_features = self.features[1:7]
         data = self.input
         self.root = find_node(data, input_features)
+        # print(self.root.name)
         self.root.depth = 1
-        node_list = [self.root]
-        for current_node in node_list:
-            if current_node.depth <= self.depth:
-                node_list.extend(find_child(data, current_node, current_node.child, input_features))
-            #     print(current_node.name, current_node.depth)
-            #     for kid in current_node.child:
-            #         print(current_node.child[kid].parent.name, kid, current_node.child[kid].name)
-            # else:
-            #     print(current_node.name, current_node.depth, current_node.label)
+        self.root.label = set_label(data)
+        # node_list = [self.root]
+        # while node_list:
+        #     current_node = node_list.pop()
+        #     if current_node.depth <= self.depth:
+        # node_list.extend(
+        find_child(data, self.root, self.root.child, input_features)
+        #     print(current_node.name, current_node.depth)
+        #     for kid in current_node.child:
+        #         print(current_node.child[kid].parent.name, kid, current_node.child[kid].name)
+        # else:
+        #     print(current_node.name, current_node.depth, current_node.label)
+
+    def find_leaf(self, row, current_node):
+        # name = current_node.name
+        node_list = [current_node]
+        # print(name)
+        for k in node_list:
+            if k.child:
+                x = row[k.name]
+                if x in k.child:
+                    if k.child[x] is not None:
+                        # k = k.child[x]
+                        node_list.append(k.child[x])
+                        # self.find_leaf(row, current_node)
+        return node_list.pop()
 
     def predict(self, data):
         data['predicted'] = '-1'
         for i in range(len(data)):
-            node_list = [self.root]
-            for item in node_list:
-                x = data.get_value(i, item.name)
-                node_list.append(item.child[x])
-                if item.label is not None:
-                    data.set_value(i, 'predicted', item.label)
-                    break
+            leaf_node = self.find_leaf(data.loc[i], self.root)
+            data.set_value(i, 'predicted', leaf_node.label)
+            # print(data.get_value(i, 'class'), data.get_value(i, 'predicted'), data.get_value(i, 'Id'))
+            # if item.child[x].label is not None:
+            #     data.set_value(i, 'predicted', item.child[x].label)
+            #     print(data.get_value(i, 'class'), data.get_value(i, 'predicted'), data.get_value(i, 'Id'))
+            #     break
         return data
+
+    def display_tree(self):
+        node_list = [self.root]
+        for node in node_list:
+            # print(node.name + "-->")
+            for kid in node.child:
+                node_list.append(node.child[kid])
+                for j in node_list:
+                    print(j.name + "-->")
+                node_list.remove(node.child[kid])
 
         # input_features.remove(root.name)
         # for key in current_node.child:
@@ -63,45 +91,67 @@ class DecisionTree:
 
 def find_node(data, predictor):
         gain, unique_values = {}, []
-        for feature in predictor:
-            entropy, entropy_sum = calculate_entropy(data), 0
-            unique_values = list(set(data[feature]))
-            for value in unique_values:
-                sub_data = data[data[feature].isin([value])]  # Splitting the data into subsets
-                # based on unique values by attribute 'feature'
-                entropy_sum += (len(sub_data)/len(data)) * calculate_entropy(sub_data)
-            gain[feature] = calculate_gain(entropy, entropy_sum)
-        sorted_gain = sorted(gain.items(), key=operator.itemgetter(1))
-        sorted_gain.reverse()
-        print(sorted_gain)
-        best_attribute = sorted_gain[0][0]
-        attribute_values = list(set(data[best_attribute]))
-        current_node = node.Node(best_attribute, attribute_values)
+        entropy = calculate_entropy(data)
+        if entropy == 0:
+            current_node = node.Node("leaf", None)
+        else:
+            for feature in predictor:
+                entropy_sum = 0
+                unique_values = list(set(data[feature]))
+                for value in unique_values:
+                    sub_data = data[data[feature].isin([value])]  # Splitting the data into subsets
+                    # based on unique values by attribute 'feature'
+                    entropy_sum += (len(sub_data)/len(data)) * calculate_entropy(sub_data)
+                gain[feature] = calculate_gain(entropy, entropy_sum)
+            sorted_gain = sorted(gain.items(), key=operator.itemgetter(1))
+            sorted_gain.reverse()
+            # print(sorted_gain)
+            best_attribute = sorted_gain[0][0]
+            attribute_values = list(set(data[best_attribute]))
+            current_node = node.Node(best_attribute, attribute_values)
         return current_node
+
+
+def return_data(data, current_node, value):
+    node_list = [current_node]
+    k = current_node
+    while k.parent is not None:
+        k = k.parent
+        node_list.append(k)
+    node_list = list(reversed(node_list))
+    for i in range(len(node_list)):
+        print(node_list[i].name)
+        sorted_node_list = sorted(node_list[i].child.items(), key=operator.itemgetter(0))
+        for j in sorted_node_list:
+            if j[1] is not None and i+1 <= len(node_list) - 1:
+                if j[1].name == node_list[i+1].name:
+                    data = data[data[node_list[i].name].isin([j[0]])]
+    last_node = node_list[len(node_list)-1].name
+    data = data[data[last_node].isin([value])]
+    return data
 
 
 def find_child(data, current_node, possible_values, features):
     edited_features = list(features)
-    parents = [current_node]
+    # parents = [current_node]
     # edited_features.remove(current_node.name)
-    for i in parents:
-        edited_features.remove(i.name)
-        if i.parent is None:
-            break
-        parents.append(i.parent)
-    child_list = []
+    # for i in parents:
+    #     edited_features.remove(i.name)
+    #     if i.parent is None:
+    #         break
+    #     parents.append(i.parent)
+    # child_list = []
     for value in possible_values:
         sub_data = data[data[current_node.name].isin([value])]
-        for attribute in parents:
-            del sub_data[attribute.name]
+        # for attribute in parents:
+        #     del sub_data[attribute.name]
         current_node.child[value] = find_node(sub_data, edited_features)
         child_node = current_node.child[value]
-        child_list.append(child_node)
         child_node.parent = current_node
         child_node.depth = current_node.depth + 1
-        if child_node.depth > dt.depth:
-            child_node.label = set_label(sub_data)
-    return child_list
+        child_node.label = set_label(sub_data)
+        if child_node.child and child_node.depth <= dt.depth:
+            find_child(sub_data, child_node, child_node.child, features)
 
 
 def set_label(data):
@@ -121,12 +171,12 @@ def calculate_entropy(data):
     p_neg = list(data[classifier]).count(0)/total
 
     if p_pos == 0:
-        x = 1
+        x = 0
     else:
         x = - p_pos*math.log(p_pos, 2)
 
     if p_neg == 0:
-        y = 1
+        y = 0
     else:
         y = - p_neg * math.log(p_neg, 2)
 
@@ -149,36 +199,18 @@ def find_error(data, a, b):
     for i in range(len(actual)):
         if not actual[i] == predicted[i]:
             error += 1
-    return error
+    return (error/len(actual))
+    # return round((1-(error/len(actual)))*100, 2)
 
 if __name__ == '__main__':
     print("Program to implement the decision tree without using package")
-    depth = eval(input("Enter the desired depth of the decision tree\n"))
+    depth = list(range(17))
+    # eval(input("Enter the desired depth of the decision tree\n"))
     path = input("Enter the filename with the full path\n")
     attributes = ['class', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'Id']
-    dt = DecisionTree(depth, process_data(path, attributes), attributes)
-    # dt.display()
-    dt.build_tree()
-    test_data = input("Enter the test file path")
-    predicted_data = dt.predict(process_data(test_data, attributes))
-    print("Number of misclassified data points:", find_error(predicted_data, 'class', 'predicted'))
-
-    # for file in train_files:
-    #     data = pd.read_table(path+'\/'+file, delim_whitespace=True, names=features, header=None)
-    #     # print(len(data))
-    #     gain = {}
-    #     for feature in input_features:
-    #         entropy, entropy_sum = calculate_entropy(data), 0
-    #         unique_values = list(set(data[feature]))
-    #         for value in unique_values:
-    #             sub_data = data[data[feature].isin([value])]  # Splitting the data into subsets
-    #             # based on unique values by attribute 'feature'
-    #             entropy_sum += (len(sub_data)/len(data)) * calculate_entropy(sub_data)
-    #         gain[feature] = calculate_gain(entropy, entropy_sum)
-    #     sorted_gain = sorted(gain.items(), key=operator.itemgetter(1))
-    #     sorted_gain.reverse()
-    #     print(sorted_gain[0][0])
-    #     break
-
-
-
+    for d in depth:
+        dt = DecisionTree(d, process_data(path, attributes), attributes)
+        dt.build_tree()
+        test_path = path.replace("train", "test")
+        predicted_data = dt.predict(process_data(test_path, attributes))
+        print(find_error(predicted_data, 'class', 'predicted'))
--- Decision_trees/node.py
+++ Decision_trees/node.py
@@ -2,8 +2,9 @@ class Node:
     def __init__(self, name, child):
         self.name = name
         self.child = {}
-        for kid in child:
-            self.child[kid] = None
+        if child is not None:
+            for kid in child:
+                self.child[kid] = None
         self.parent = None
         self.label = None
         self.depth = None
