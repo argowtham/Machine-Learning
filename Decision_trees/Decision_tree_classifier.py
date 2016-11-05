cffed87ad7da9e13d3d16f585c7a5216304e942f
argowtham <gowtayna@indiana.edu>
2016-11-05 19:29:53 -0400

    Binary split decision tree for multi class classifier

---
+++ Decision_trees/Decision_tree_classifier.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn import tree
+import numpy as np
+
+
+def evaluate(array1, array2):
+    correct, wrong = 0, 0
+    for i, j in zip(array1, array2):
+        if i == j:
+            correct += 1
+        else:
+            wrong += 1
+    print("Accuracy is {}.".format(correct / len(array1)))
+    print("Error rate is {}.".format(wrong / len(array1)))
+
+
+if __name__ == "__main__":
+    data = pd.read_csv(
+        "C:\/Users\Gowtham\Box Sync\IU\Fall'16\Applied Machine Learning\Homeworks\PA2\mushrooms\/agaricuslepiotatrain1.csv",
+        sep=',', header='infer')
+    test_data = pd.read_csv(
+        "C:\/Users\Gowtham\Box Sync\IU\Fall'16\Applied Machine Learning\Homeworks\PA2\mushrooms\/agaricuslepiotatest1.csv",
+        sep=',', header='infer')
+    Y = data['bruises?_bruises']
+    X = data.drop(data.columns[20:22], axis=1)
+    X_test = test_data.drop(test_data.columns[20:22], axis=1)
+
+    model = tree.DecisionTreeClassifier(criterion='entropy')
+    model.fit(X, Y)
+    predicted = model.predict(X_test)
+    actual = np.array(test_data['bruises?_bruises'])
+    evaluate(predicted, actual)
